61fc8eeecaaa67ed343fc9719e38acc1d0537691
Thanakornnam <45282559+Thanakornnam@users.noreply.github.com>
2019-05-10 01:40:43 +0700

    Update line.php

--- line.php
+++ line.php
@@ -4,11 +4,9 @@
 function send_LINE($msg){
  $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
 
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-        //'text' => $text
-      ];
+       $messages = [];
+      $messages['replyToken'] = $replyToken;
+      $messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
 
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
