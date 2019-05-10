d4c4368edd39413b7ef81ecd2eac6c49b3569520
Thanakornnam <45282559+Thanakornnam@users.noreply.github.com>
2019-05-10 22:55:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
-   send_LINE($events['ESP']);
+   send_LINE($events['อับดุล']);
    echo "OK";
 }
 
