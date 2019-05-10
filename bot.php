4c47d85b9bb54cf385ba5bc5ce46f9d1b41cfd89
Thanakornnam <45282559+Thanakornnam@users.noreply.github.com>
2019-05-10 10:59:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,6 +10,29 @@ if (!is_null($events['ESP'])) {
    send_LINE($events['ESP']);
    echo "OK";
 }
+
+
+
+
+$events = json_decode($content, true);
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    $userMessage = strtolower($userMessage);
+    switch ($typeMessage){
+        case 'text':
+            switch ($userMessage) {
+                case "t":
+                    $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    break;
+			    
+			    
+			    
+			    
+			    
 if (!is_null($events['events'])) {
    echo "line bot";
    // Loop through each event
